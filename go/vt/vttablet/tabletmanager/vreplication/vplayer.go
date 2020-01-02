503bfb1fe341fee3cb3e50d64562fef08053ef9c
Rohit Nayak <rohit@planetscale.com>
2020-01-02 11:38:12 +0100

    Added /debug/vrlog endpoint. Initial commit, untested
    Signed-off-by: Rohit Nayak <rohit@planetscale.com>

--- go/vt/vttablet/tabletmanager/vreplication/vplayer.go
+++ go/vt/vttablet/tabletmanager/vreplication/vplayer.go
@@ -191,7 +191,10 @@ func (vp *vplayer) applyRowEvent(ctx context.Context, rowEvent *binlogdatapb.Row
 	}
 	for _, change := range rowEvent.RowChanges {
 		_, err := tplan.applyChange(change, func(sql string) (*sqltypes.Result, error) {
-			return vp.vr.dbClient.ExecuteWithRetry(ctx, sql)
+			stats := NewVrLogStats(ctx, "RowChange")
+			result, err := vp.vr.dbClient.ExecuteWithRetry(ctx, sql)
+			stats.Record(sql)
+			return result, err
 		})
 		if err != nil {
 			return err
@@ -304,6 +307,7 @@ func hasAnotherCommit(items [][]*binlogdatapb.VEvent, i, j int) bool {
 }
 
 func (vp *vplayer) applyEvent(ctx context.Context, event *binlogdatapb.VEvent, mustSave bool) error {
+	stats := NewVrLogStats(ctx, event.String())
 	switch event.Type {
 	case binlogdatapb.VEventType_GTID:
 		pos, err := mysql.DecodePosition(event.Gtid)
@@ -349,6 +353,8 @@ func (vp *vplayer) applyEvent(ctx context.Context, event *binlogdatapb.VEvent, m
 			return err
 		}
 		vp.tablePlans[event.FieldEvent.TableName] = tplan
+		stats.Record(fmt.Sprintf("%v", event.FieldEvent))
+
 	case binlogdatapb.VEventType_INSERT, binlogdatapb.VEventType_DELETE, binlogdatapb.VEventType_UPDATE, binlogdatapb.VEventType_REPLACE:
 		// This is a player using stament based replication
 		if err := vp.vr.dbClient.Begin(); err != nil {
@@ -358,6 +364,7 @@ func (vp *vplayer) applyEvent(ctx context.Context, event *binlogdatapb.VEvent, m
 		if err := vp.applyStmtEvent(ctx, event); err != nil {
 			return err
 		}
+		stats.Record(fmt.Sprintf(event.Dml))
 	case binlogdatapb.VEventType_ROW:
 		// This player is configured for row based replication
 		if err := vp.vr.dbClient.Begin(); err != nil {
@@ -366,6 +373,7 @@ func (vp *vplayer) applyEvent(ctx context.Context, event *binlogdatapb.VEvent, m
 		if err := vp.applyRowEvent(ctx, event.RowEvent); err != nil {
 			return err
 		}
+		stats.Record(fmt.Sprintf("%v", event.RowEvent))
 	case binlogdatapb.VEventType_OTHER:
 		// Just update the position.
 		posReached, err := vp.updatePos(event.Timestamp)
@@ -407,6 +415,7 @@ func (vp *vplayer) applyEvent(ctx context.Context, event *binlogdatapb.VEvent, m
 			if _, err := vp.vr.dbClient.ExecuteWithRetry(ctx, event.Ddl); err != nil {
 				return err
 			}
+			stats.Record(fmt.Sprintf("%v", event.Ddl))
 			posReached, err := vp.updatePos(event.Timestamp)
 			if err != nil {
 				return err
@@ -418,6 +427,7 @@ func (vp *vplayer) applyEvent(ctx context.Context, event *binlogdatapb.VEvent, m
 			if _, err := vp.vr.dbClient.ExecuteWithRetry(ctx, event.Ddl); err != nil {
 				log.Infof("Ignoring error: %v for DDL: %s", err, event.Ddl)
 			}
+			stats.Record(fmt.Sprintf("%v", event.Ddl))
 			posReached, err := vp.updatePos(event.Timestamp)
 			if err != nil {
 				return err
@@ -466,6 +476,7 @@ func (vp *vplayer) applyEvent(ctx context.Context, event *binlogdatapb.VEvent, m
 			}
 			return io.EOF
 		}
+		stats.Record(fmt.Sprintf("%v", event.Journal))
 		return io.EOF
 	case binlogdatapb.VEventType_HEARTBEAT:
 		// No-op: heartbeat timings are calculated in outer loop.
